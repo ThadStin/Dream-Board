8dd0b35384bf06625e02628aa7fdf72bab36cade
Thad Stinson <bootytee@Thads-iMac.local>
2020-06-11 10:21:55 -0500

    all functioning except pic and achieved

--- client/src/pages/DreamsList.jsx
+++ client/src/pages/DreamsList.jsx
@@ -9,6 +9,45 @@ import 'react-table-6/react-table.css'
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
 `
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor: pointer;
+`
+const Delete = styled.div`
+    color: #ff0000;
+    cursor: pointer;
+`
+class UpdateDream extends Component {
+    updateUser = event => {
+        event.preventDefault()
+
+        window.location.href = `/dreams/update/${this.props.id}`
+    }
+
+    render() {
+        return <Update onClick={this.updateUser}>Update</Update>
+    }
+}
+
+class DeleteDream extends Component {
+    deleteUser = event => {
+        event.preventDefault()
+
+        if (
+            window.confirm(
+                `Do you want to delete this dream permanently?`, //${this.props.id}
+            )
+        ) {
+            api.deleteDreamById(this.props.id)
+            window.location.reload()
+        }
+    }
+
+    render() {
+        return <Delete onClick={this.deleteUser}>Delete</Delete>
+    }
+}
+
 
 class DreamsList extends Component {
     constructor(props) {
@@ -36,11 +75,11 @@ class DreamsList extends Component {
         console.log('TCL: DreamsList -> render -> dreams', dreams)
 
         const columns = [
-            {
-                Header: 'ID',
-                accessor: '_id',
-                filterable: true,
-            },
+            // {
+            //     Header: 'ID',
+            //     accessor: '_id',
+            //     filterable: true,
+            // },
             {
                 Header: 'Name',
                 accessor: 'name',
@@ -55,6 +94,13 @@ class DreamsList extends Component {
                 Header: 'Pic',
                 accessor: 'pic',
                 filterable: true,
+                Cell: function(props) {
+                  return (
+                    <span>
+                      <img src={`${props.pic}`} alt="dream pic" height="100px" width="100px" />
+                    </span> // giving undefined as img src!!!
+                  )
+                }
             },
             {
                 Header: 'Location',
@@ -65,6 +111,36 @@ class DreamsList extends Component {
                 Header: 'Achieved',
                 accessor: 'achieved',
                 filterable: true,
+                Cell: function(props) {
+                  return (
+                    <span>
+                      <input type="checkbox" name="achieved" value="achieved"></input>
+                      <label for="achieved">Achieved</label>
+                    </span> // checkboxes there but not functioning!!!!
+                  )
+                }
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props) {
+                    return (
+                        <span>
+                            <DeleteDream id={props.original._id} />
+                        </span>
+                    )
+                },
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props) {
+                    return (
+                        <span>
+                            <UpdateDream id={props.original._id} />
+                        </span>
+                    )
+                },
             },
             // {
             //     Header: 'Time',
